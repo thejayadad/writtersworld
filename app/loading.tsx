9e594b7a3ec66b283895e6212b9b9b4ed8e3dc7a
thejayadad <74875595+thejayadad@users.noreply.github.com>
2025-01-23 12:26:40 -0500

    update

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div
+    className='h-full flex flex-col justify-center items-center'
+    >loading...</div>
+  )
+}
+
+export default loading
